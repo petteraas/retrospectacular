459217fda70e469398005d933b8e7b36092a5a74
Petter Aas <aaspet@gmail.com>
2013-12-14 10:38:50 +0100

    Connect swagger with the api

--- service/app.js
+++ service/app.js
@@ -1,9 +1,12 @@
 var routes = require('./routes'),
     express = require('express'),
+    swagger = require('swagger-node-express'),
     api = express();
 
 api.use(express.methodOverride());
 api.use(express.json());
+
+swagger.setAppHandler(api);
 
 routes.setup(api);
 
