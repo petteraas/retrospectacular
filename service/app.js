bc8ca14b50568d769e70aa0e211dd24e98d5d6d8
MrFjellstad <kent@fjellstad.name>
2014-01-04 11:26:06 +0100

    First go at access control with Google

--- service/app.js
+++ service/app.js
@@ -5,8 +5,8 @@ var routes = require('./routes'),
 api.use(express.methodOverride());
 api.use(express.json());
 
-routes.setup(api);
+routes.setup(api,express);
 
 api.use(express.logger('dev'));
-
+api.use(express.static("../static/app"));
 api.listen(3000);
