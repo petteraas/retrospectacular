a1ba01fe024b665205e2f2a4e9d123a3ecd8e679
Petter Aas <petter.aas@fronter.com>
2014-01-30 16:27:16 +0100

    Linted retrospectives.js

--- static/app/scripts/controllers/retrospectives.js
+++ static/app/scripts/controllers/retrospectives.js
@@ -7,44 +7,44 @@ angular.module('retrospectApp')
         'retrospectives',
         'LocaleDateTime',
 
-    function ($scope, $location, retrospectives, LocaleDateTime) {
-        $scope.retrospectives = [];
-        $scope.newRetrospective = {};
-        $scope.limit = 10000;
-        $scope.page = 1;
+        function ($scope, $location, retrospectives, LocaleDateTime) {
+            $scope.retrospectives = [];
+            $scope.newRetrospective = {};
+            $scope.limit = 10000;
+            $scope.page = 1;
 
-        $scope.deleteRetrospective = function(retrospective) {
-            var index = $scope.retrospectives.indexOf(retrospective);
+            $scope.deleteRetrospective = function(retrospective) {
+                var index = $scope.retrospectives.indexOf(retrospective);
 
-            retrospectives.delete({'retroId': retrospective.id}, function () {
-                console.log('deleted retrospective');
-                $scope.retrospectives.splice(index, 1);
-            });
-        };
-
-        $scope.saveRetrospective = function () {
-            var retrospective;
-
-            if ($scope.retroAddForm.$valid) {
-                retrospective = angular.copy($scope.newRetrospective);
-
-                // Sending plan data to the service
-                retrospectives.save($scope.newRetrospective, function (savedRetrospective) {
-                    $scope.retrospectives.push(savedRetrospective);
-                    $location.path('/retrospectives/' + savedRetrospective.id);
-
-                }, function (error) {
-                    console.error('Error saving retrospective', error);
+                retrospectives.delete({'retroId': retrospective.id}, function () {
+                    console.log('deleted retrospective');
+                    $scope.retrospectives.splice(index, 1);
                 });
+            };
 
-            } else {
-                console.warn('Errors in form data');
-            }
-        };
+            $scope.saveRetrospective = function () {
+                var retrospective;
 
-        // this throws an error with cross domain
-        retrospectives.get({'limit': $scope.limit, 'page': $scope.page},function (response) {
-            $scope.retrospectives = LocaleDateTime.localizeResults(response.results);
-        });
-    }
-]);
+                if ($scope.retroAddForm.$valid) {
+                    retrospective = angular.copy($scope.newRetrospective);
+
+                    // Sending plan data to the service
+                    retrospectives.save($scope.newRetrospective, function (savedRetrospective) {
+                        $scope.retrospectives.push(savedRetrospective);
+                        $location.path('/retrospectives/' + savedRetrospective.id);
+
+                    }, function (error) {
+                        console.error('Error saving retrospective', error);
+                    });
+
+                } else {
+                    console.warn('Errors in form data');
+                }
+            };
+
+            // this throws an error with cross domain
+            retrospectives.get({'limit': $scope.limit, 'page': $scope.page},function (response) {
+                $scope.retrospectives = LocaleDateTime.localizeResults(response.results);
+            });
+        }
+    ]);
