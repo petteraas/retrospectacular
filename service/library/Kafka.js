4e8c1ab5f9261c22633926ae8e4d829fc0ac76e5
Petter Aas <petter.aas@fronter.com>
2014-02-11 13:46:49 +0100

    Started on a simple wrapper around kafka-node

---
+++ service/library/Kafka.js
@@ -0,0 +1,30 @@
+var config = require('../config').Config,
+    kafka = require('kafka-node'),
+    Producer = kafka.Producer,
+    client = new kafka.Client(config.kafka.connectionString, config.kafka.clientId),
+    producer = new Producer(client),
+    producerReady = false;
+
+producer.on('ready', function () {
+    producerReady = true;
+});
+
+producer.on('error', function (err) {
+    console.log('producer error', err);
+});
+
+exports.sendMessage = function (type, message) {
+    if (producerReady) {
+        message.type = type;
+        var payloads = [{
+            topic: config.kafka.topic,
+            messages: JSON.stringify(message)
+        }];
+        producer.send(payloads, function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            return data;
+        });
+    }
+};
