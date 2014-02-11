d367794524cf49998488386b0c37b298356b4c4e
Petter Aas <petter.aas@fronter.com>
2014-02-11 15:51:36 +0100

    Updated app.js according to newer express

--- service/app.js
+++ service/app.js
@@ -1,13 +1,22 @@
 var routes = require('./routes'),
+    sockets = require('./sockets'),
+
     express = require('express'),
     api = express(),
+    http = require('http'),
+    server = http.createServer(api),
+
+    io = require('socket.io').listen(server),
+
     config = require('./config').Config;
 
 api.use(express.methodOverride());
 api.use(express.json());
 
 routes.setup(api);
+sockets.setup(io);
+
 
 api.use(express.logger('dev'));
 
-api.listen(config.app.port, config.app.host);
+server.listen(config.app.port, config.app.host);
