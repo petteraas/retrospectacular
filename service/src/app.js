84cf2650906689ff1bb83f6f3398256307d29b1c
Petter Aas <petter.aas@fronter.com>
2014-02-24 15:49:43 +0100

    Testing Winston-Logstash

--- service/src/app.js
+++ service/src/app.js
@@ -1,13 +1,37 @@
 var routes = require('./routes'),
+    winston = require('winston'),
     express = require('express'),
     api = express(),
     config = require('../config').Config;
+
+require('winston-logstash');
+
+
+
+winston.add(winston.transports.Logstash, {
+    port: config.logstash.port,
+    host: config.logstash.host,
+    node_name: config.logstash.node_name,
+    silent: false
+});
+
+var winstonStream = {
+        write: function (message) {
+            winston.log('info', message, {
+                'access_log': 'true'
+            });
+        }
+    };
+
+api.use(express.logger({
+    stream: winstonStream
+}));
 
 api.use(express.methodOverride());
 api.use(express.json());
 
 routes.setup(api);
 
-api.use(express.logger('dev'));
+//api.use(express.logger('dev'));
 
 api.listen(config.app.port, config.app.host);
