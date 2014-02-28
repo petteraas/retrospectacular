b3563b16820e154553e5440be8e56de5d63fb8a5
Petter Aas <aaspet@gmail.com>
2014-02-28 23:11:41 +0100

    Use SERVICE_URL when checking access

--- static/app/scripts/directives/fuiAccess.js
+++ static/app/scripts/directives/fuiAccess.js
@@ -1,18 +1,20 @@
 'use strict';
 
 angular.module('retrospectApp')
-  .directive('fuiAccess',[ 'isLoggedIn', function (isLoggedIn) {
+  .directive('fuiAccess',[ 'isLoggedIn', 'SERVICE_URL', function (isLoggedIn, SERVICE_URL) {
     return {
       templateUrl: 'views/directives/fuiAccess.html',
       restrict: 'EA',
       link: function postLink(scope, element, attrs) {
+        SERVICE_URL = SERVICE_URL.replace(/\\:/, ':');
+
         scope.text = 'Sign In with Google';
-        scope.url = '/auth/google';
+        scope.url = SERVICE_URL + '/auth/google';
         isLoggedIn.get(function (response) {
           scope.elements = response;
           if (scope.elements.displayName) {
             scope.text = scope.elements.displayName;
-            scope.url = '/auth/logout';
+            scope.url = SERVICE_URL + '/auth/logout';
             // This is not ideal. The page only shows 200
           }
         });
