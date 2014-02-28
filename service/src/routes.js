3cafa76de1ec9141810eddae98569b5d6dc085f8
Petter Aas <aaspet@gmail.com>
2014-02-28 23:11:12 +0100

    Either load existing user or create new then load it upon logging with google.

--- service/src/routes.js
+++ service/src/routes.js
@@ -1,7 +1,11 @@
 'use strict';
 
-var retrospectives = require('./models/retrospectives'),
+var _ = require('lodash'),
+    retrospectives = require('./models/retrospectives'),
     tickets = require('./models/tickets'),
+    users = require('./models/users'),
+    config = require('../config').Config,
+    db = require('./wrapper'),
 
     allowCrossDomain = function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
@@ -12,10 +16,11 @@ var retrospectives = require('./models/retrospectives'),
     passport = require('passport'),
     googleStrategy = require('passport-google').Strategy,
     auth = function(req, res, next) {
-        if (!req.isAuthenticated())
+        if (!req.isAuthenticated()) {
             res.send(401);
-        else
+        } else {
             next();
+        }
     };
 
 // Passport session setup.
@@ -33,37 +38,6 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-
-passport.use(new googleStrategy({
-    returnURL: 'http://localhost:3000/auth/google/return',
-    realm: 'http://localhost:3000/'
-}, function(identifier, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-
-
-// Passport session setup.
-// To support persistent login sessions, Passport needs to be able to
-// serialize users into and deserialize users out of the session. Typically,
-// this will be as simple as storing the user ID when serializing, and finding
-// the user by ID when deserializing. However, since this example does not
-// have a database of user records, the complete Google profile is serialized
-// and deserialized.
-    profile.identifier = identifier;
-    return done(null, profile);
-    });
-}));
-
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-
 passport.use(new googleStrategy({
     returnURL: 'http://localhost:3000/auth/google/return',
     realm: 'http://localhost:3000/'
@@ -74,17 +48,43 @@ passport.use(new googleStrategy({
         // represent the logged-in user. In a typical application, you would want
         // to associate the Google account with a user record in your database,
         // and return that user instead.
-        profile.identifier = identifier;
-        return done(null, profile);
-        });
-    }
-));
 
+        users.getUserByName(profile.displayName)
+          .then(function (user) {
+            if (_.isUndefined(user)) {
+              db.post(profile).ofType('user').into(config.db.index)
+                .then(function (response) {
+                  console.log('post response: ', response);
+                  return done(null, response);
+                })
+                .fail(function (err) {
+                  console.log('post err: ', err);
+                });
+            } else {
+              console.log('found user: ', user);
+              return done(null, user);
+            }
+          })
+          .fail(function (err) {
+            console.log('getUserByName err', err);
+          });
+    });
+}));
 
 
 exports.setup = function (api, express) {
+    var RedisStore = require('connect-redis')(express),
+        redis = require('redis').createClient(config.redis.port, config.redis.host);
+
     api.use(express.cookieParser());
-    api.use(express.session({ secret: 'THIS IS A SECRET' }));
+    api.use(express.session({
+      secret: 'THIS IS A SECRET',
+      store: new RedisStore({
+        host: config.redis.host,
+        port: config.redis.port,
+        client: redis
+      })
+    }));
   // Initialize Passport! Also use passport.session() middleware, to support
   // persistent login sessions (recommended).
     api.use(passport.initialize());
@@ -114,4 +114,9 @@ exports.setup = function (api, express) {
     api.get('/auth/loggedin', function (req, res) {
         res.send(req.isAuthenticated() ? req.user : '0');
     });
+
+    api.get('/auth/profile', function (req, res) {
+      res.json(req.session);
+    });
+
 };
