65ea9c051afa58558b367e6b26168357144182b3
Petter Aas <aaspet@gmail.com>
2014-03-17 19:52:52 +0100

    Exit with either success or failure. Print some logs. Fixes 55

--- scripts/backup/es-backup.sh
+++ scripts/backup/es-backup.sh
@@ -10,6 +10,18 @@ day=$(date +"%d")
 path=$base/$year/$month/$day
 /bin/mkdir -p $path
 
-/usr/bin/env esdump --url http://localhost:9200/ --indexes $original --bzip2 --file "${path}/${backup}.bz2"
+dumpFile=$path/$backup.bz2
+
+ERROR=$(/usr/bin/env esdump --url http://host1:9200/ --indexes $original --bzip2 --file $dumpFile 2>&1)
+RESULT=$?
+if [ $RESULT -eq 0 ];
+then
+    echo "Dumped index ${original} to file ${dumpFile}";
+    exit 0;
+else
+    echo "Dumping index ${original} failed with error: \n\n${ERROR}\n\n";
+    exit 1;
+fi
+
 
 #/usr/bin/env esimport --url http://localhost:9200 --index $backup --file "${path}/${backup}.bz2"
